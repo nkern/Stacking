cc8b0f3a2ddc490f4bf3d950c07ff9630b7f6042
Nicholas S. Kern <nkern@umich.edu>
2014-04-10 06:21:54 -0400

    modified:   README.md 	modified:   bs_analysis.py 	modified:   table_run.sh

--- bs_analysis.py
+++ bs_analysis.py
@@ -9,8 +9,26 @@ for (j,k) in zip(np.arange(1,50),[2,5,10,15,25,50,100]*7):
 			print 'j =',j,',i =',i
 
 
+## Create BIN arrays
+mass_array = M_crit200
+hvd_array = HVD
+rad_array = R_crit200
+mass_array = HaloData_match[0]
+hvd_array = HaloData_match[5]
+rad_array = HaloData_match[1]
+BIN_M200,BIN_HVD,BIN_R200 = [],[],[]
+for i in range(2100/line_num):
+	BIN_M200.append(np.mean(mass_array[i*line_num:(i+1)*line_num]))
+	BIN_HVD.append(np.mean(hvd_array[i*line_num:(i+1)*line_num]))
+	BIN_R200.append(np.mean(rad_array[i*line_num:(i+1)*line_num]))
+
+BIN_M200 = np.array(BIN_M200)
+BIN_R200 = np.array(BIN_R200)
+BIN_HVD = np.array(BIN_HVD)
+
+
 ## Make Ensemble Mass 1-1 plot
-mp.loglog([BIN_M200[0],BIN_M200[-1]],[BIN_M200[0],BIN_M200[-1]],'b')
+mp.loglog([BIN_M200.min(),BIN_M200.max()],[BIN_M200.min(),BIN_M200.max()],'b')
 mp.loglog(BIN_M200,ENS_CAUMASS,'ko',alpha=.7)
 mp.xlim(6e13, 1.3e15)
 mp.ylim(3.5e13,1.3e15)
@@ -19,6 +37,7 @@ mp.ylabel('Ensemble Mass Estimate',fontsize=15)
 mp.title('1-1 Correlation, Ngal='+str(gal_num)+', LOS='+str(line_num)+', ENS Richness='+str(line_num*gal_num))
 mp.figtext(.65,.15,'Cell #'+str(cell_num)+'\nMethod #'+str(varib['method_num'])+'\nBias = '+str(np.around(ens_mbias,2))+'\nScatter = '+str(np.around(ens_mscat,2)))
 mp.show()
+
 
 ## Make Ensemble Mass_Est 1-1 plot
 mp.loglog([BIN_M200[0],BIN_M200[-1]],[BIN_M200[0],BIN_M200[-1]],'b')
@@ -43,8 +62,8 @@ mp.title('1-1 Correlation, Ngal='+str(gal_num)+', LOS='+str(line_num)+', ENS Ric
 mp.figtext(.7,.2,'Cell #'+str(cell_num)+'\nMethod #'+str(varib['method_num'])+'\nBias = '+str(np.around(ens_vbias,2))+'\nScatter = '+str(np.around(ens_vscat,2)))
 mp.show()
 
-# Make LOS Mass 1-1 Plot
 
+# Make LOS Mass 1-1 Plot
 mp.loglog([M_crit200[0],M_crit200[-1]],[M_crit200[0],M_crit200[-1]],'b')
 mp.loglog(M_crit200[0:halo_num],LOS_CAUMASS.ravel(),'ko',alpha=.7)
 mp.xlim(6e13, 1.3e15)
@@ -55,9 +74,20 @@ mp.title('1-1 Correlation, Ngal='+str(gal_num)+', LOS='+str(line_num)+', ENS Ric
 mp.figtext(.65,.15,'Cell #'+str(cell_num)+'\nMethod #'+str(varib['method_num'])+'\nBias = '+str(np.around(los_mbias,2))+'\nScatter = '+str(np.around(los_mscat,2)))
 mp.show()
 
+# Ensemble Phase Space
+i = 20
+mp.plot(ENS_R[i],ENS_V[i],'ko',alpha=.8)
+mp.plot(x_range,ENS_CAUSURF[i],'b',linewidth=2)
+mp.xlabel('cluster-centric radius (Mpc)',fontsize=15)
+mp.ylabel('los velocity (km/s)',fontsize=15)
+mp.title('Ensemble '+str(i)+' Phase Space')
+mp.xlim(0,BIN_R200[i]*1.5)
+mp.axvline(BIN_R200[i],ymin=0,ymax=.1,c='r')
+mp.show()
 
+### MASS MIXING PLOTS ###
 
-
+# B
 
 
 
--- table_run.sh
+++ table_run.sh
@@ -18,8 +18,8 @@ line_num=(2 5 10 15 25 50 100)			# Line of Sight Number
 gal_num=(5 10 15 25 50 100 150)			# Ngal number
 halo_num=2100					# Number of Halos in Sample
 method_num=0					# Ensemble Build Method
-table_num=2					# Version of entire run table
-data_loc="binstack_run_table$table_num"		# Highest Directory for Data
+table_num=1					# Version of entire run table
+data_loc="mass_mix/mm_0.50_run_table$table_num"	# Highest Directory for Data
 
 ## Go To Stacking Directory
 cd /nfs/christoq_ls/nkern/Stacking
@@ -49,7 +49,7 @@ fi
 # Sub Directory Check (including LOS)
 for i in ${cell_num[*]}
 do
-	dir="bs_m0_run"$i
+	dir="mm_m0_run"$i
 	if [ -d $data_loc\/$dir ]
 		then
 		echo -n
@@ -80,7 +80,7 @@ do
 		# Define Constants
 		job_array=(13 13 13 13 13 13 20)
 		let "clus_num=$halo_num/(${job_array[$j]}+1)/${line_num[$j]}"
-		write_loc="bs_m0_run"${cell_num[$k]}
+		write_loc="mm_m0_run"${cell_num[$k]}
 
 		# Submit FLUX JOB for Ensembles
 		_clus_num="$clus_num"
@@ -91,13 +91,14 @@ do
 		_job_array="${job_array[$j]}"
 		_data_loc="$data_loc"
 		_write_loc="$write_loc"
-		sed -e "s/@@data_loc@@/$_data_loc/g;s/@@write_loc@@/$_write_loc/g;s/@@job_array@@/$_job_array/g;s/@@clus_num@@/$_clus_num/g;s/@@gal_num@@/$_gal_num/g;s/@@line_num@@/$_line_num/g;s/@@method_num@@/$_method_num/g;s/@@cell_num@@/$_cell_num/g;s/@@table_num@@/$table_num/g;s/@@run_los@@/0/g" < table_flux_stack_pbs.sh > $_data_loc/$_write_loc/script.sh
+		
+		sed -e "s/@@write_loc@@/$_write_loc/g;s/@@job_array@@/$_job_array/g;s/@@clus_num@@/$_clus_num/g;s/@@gal_num@@/$_gal_num/g;s/@@line_num@@/$_line_num/g;s/@@method_num@@/$_method_num/g;s/@@cell_num@@/$_cell_num/g;s/@@table_num@@/$table_num/g;s/@@run_los@@/0/g" < table_flux_stack_pbs.sh > $_data_loc/$_write_loc/script.sh
 
 		# Change run_los = True if line_num == 100
 		let "a=${cell_num[$k]}%7"
 		if [ $a == 0 ]
 			then
-			sed -e "s/@@data_loc@@/$_data_loc/g;s/@@write_loc@@/$_write_loc/g;s/@@job_array@@/$_job_array/g;s/@@clus_num@@/$_clus_num/g;s/@@gal_num@@/$_gal_num/g;s/@@line_num@@/$_line_num/g;s/@@method_num@@/$_method_num/g;s/@@cell_num@@/$_cell_num/g;s/@@table_num@@/$table_num/g;s/@@run_los@@/1/g" < table_flux_stack_pbs.sh > $_data_loc/$_write_loc/script.sh
+			sed -e "s/@@write_loc@@/$_write_loc/g;s/@@job_array@@/$_job_array/g;s/@@clus_num@@/$_clus_num/g;s/@@gal_num@@/$_gal_num/g;s/@@line_num@@/$_line_num/g;s/@@method_num@@/$_method_num/g;s/@@cell_num@@/$_cell_num/g;s/@@table_num@@/$table_num/g;s/@@run_los@@/1/g" < table_flux_stack_pbs.sh > $_data_loc/$_write_loc/script.sh
 		fi
 
 		# Submit Script to FLUX via qsub
